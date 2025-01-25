145675736ad0ebd4631168aeaa17db0db00cbc8d
pradeep <pradeep@gmail.com>
2025-01-25 12:02:24 +0530

    committing the changes to the master branch

--- Feature1.java
+++ Feature1.java
@@ -2,4 +2,8 @@ public class Feature1 {
     public static void main(String[] args) {
         System.out.println("Feature1");
     }
+
+    public void feature1() {
+        System.out.println("Feature1");
+    }
 }
